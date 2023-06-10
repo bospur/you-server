f9b37c497408d3c27f75e68d5dea6e8ec1912724
Bospur <Bospobob@yandex.ru>
2023-06-10 15:41:52 +0300

    Fix port

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,13 @@ import { config } from "dotenv";
 import express from "express";
 const app = express();
 config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.send("The sedulous hyena ate the antelope!");
+  res.send("The sedulous hyena ate the antelope!");
 });
 app.get("/info", (req, res) => {
-    res.send("The info");
+  res.send("The info");
 });
 app.listen(port, () => {
-    return console.log(`server is listening on http://localhost:${port}`);
+  return console.log(`server is listening on http://localhost:${port}`);
 });
