f9056e7e0c430615cfe14c315c50bf95b7cfa57e
Ivan <i.semenov@eputs.org>
2023-09-07 16:19:46 +0300

    Add env and my bd config

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
 import path from "path";
+import mysql from "mysql2";
 
 import { config } from "dotenv";
 import express from "express";
 
 const app = express();
 config();
-
-const port = 80;
+const { PORT, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 app.use("/", express.static(path.resolve("../you-space/dist")));
 
-app.get("/info", (req, res) => {
-  res.send("The info");
+app.get("/api/purchase", (req, res) => {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: Number(DB_PORT),
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+  });
+
+  connection.connect((err) => {
+    if (err) return console.log(err);
+  });
+
+  connection.query("SELECT * FROM purchase", (err, result) => {
+    res.send(result);
+  });
+
+  connection.end();
 });
 
-app.listen(port, () => {
-  return console.log(`server is listening on http://localhost:${port}`);
+app.listen(PORT, () => {
+  return console.log(`server is listening on http://localhost:${PORT}`);
 });
