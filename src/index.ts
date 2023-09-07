ba6d667bcb743d4584a8c3f9a6168ed5a7744a6a
Ivan <i.semenov@eputs.org>
2023-09-07 16:41:39 +0300

    Add post request

--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,40 @@ const { PORT, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 app.use("/", express.static(path.resolve("../you-space/dist")));
 
-app.get("/api/purchase", (req, res) => {
-  const connection = mysql.createConnection({
-    host: "localhost",
-    port: Number(DB_PORT),
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME,
-  });
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: Number(DB_PORT),
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+});
 
+const urlencodedParser = express.urlencoded({ extended: false });
+
+app.get("/api/purchase", (req, res) => {
   connection.connect((err) => {
     if (err) return console.log(err);
   });
 
   connection.query("SELECT * FROM purchase", (err, result) => {
+    res.status(200);
     res.send(result);
   });
 
   connection.end();
+});
+
+app.post("/purchase", urlencodedParser, function (request, response) {
+  const body = request.body;
+  if (!body) return response.sendStatus(400);
+
+  connection.query(`INSERT INTO purchase(category, amount)
+    VALUES (${body.category}, ${body.amount})`);
+
+  response.status(200);
+  response.send({
+    message: "Запись успешно добавлена",
+  });
 });
 
 app.listen(PORT, () => {
